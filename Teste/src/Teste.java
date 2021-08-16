5a9fd42f7c25811d885a96ad5dadb7105d315000
Davi Gomes de Sousa <80433922+DaviGsSousa@users.noreply.github.com>
2021-08-16 15:08:15 -0300

    Add files via upload

---
+++ Teste/src/Teste.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Teste {
+	public static void main(String[] args){
+		double num1 = 0;
+		double num2 = 0;
+		double resultado = 0;
+		int opcao = 0;
+		
+		Scanner leitor = new Scanner(System.in);
+		System.out.print("Escolha uma opção: 1 - soma; 2 - subtração; 3 - divisão; 4- multiplicação");
+		opcao = leitor.nextInt();
+		
+		while (opcao > 4 || opcao < 1) {
+			System.out.println("Você digitou um número de opção inválido");
+			System.out.print("Escolha uma opção: 1 - soma; 2 - subtração; 3 - divisão; 4- multiplicação");
+			opcao = leitor.nextInt();
+		}
+		
+		System.out.print("Digite o primeiro número: ");
+		num1 = leitor.nextDouble();
+		System.out.print("Digite o segundo número: ");
+		num2 = leitor.nextDouble();
+		
+		leitor.close();
+		
+		switch (opcao) {
+		case 1:
+			resultado = num1 + num2;
+			break;
+			
+		case 2:
+			resultado = num1 - num2;
+			break;
+			
+		case 3:
+			resultado = num1 / num2;
+			break;
+			
+		case 4:
+			resultado = num1 * num2;
+			break;
+		}
+		
+		System.out.print("resultado = " + resultado);
+	}
+}
