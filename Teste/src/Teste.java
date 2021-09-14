c24df3d4c2b8a10b6ed0aba6640dcad17d69c38a
DaviGsSousa <davigsousa@gmail.com>
2021-09-13 22:08:50 -0300

    adding loops

--- Teste/src/Teste.java
+++ Teste/src/Teste.java
@@ -6,42 +6,69 @@ public class Teste {
 		double num2 = 0;
 		double resultado = 0;
 		int opcao = 0;
-		
+		boolean repeat = true;
+		int continuarC = 0;
 		Scanner leitor = new Scanner(System.in);
-		System.out.print("Escolha uma opção: 1 - soma; 2 - subtração; 3 - divisão; 4- multiplicação");
-		opcao = leitor.nextInt();
 		
-		while (opcao > 4 || opcao < 1) {
-			System.out.println("Você digitou um número de opção inválido");
-			System.out.print("Escolha uma opção: 1 - soma; 2 - subtração; 3 - divisão; 4- multiplicação");
+		System.out.println("Deseja executar alguma operação? yes = 1/no = 2" );
+		continuarC = leitor.nextInt();
+		
+		if(continuarC == 1) {
+			repeat = true;
+		} else {
+			System.out.println("vish kk");
+			repeat = false;
+		}
+		while(repeat == true) {
+			System.out.println("+-------------------+");
+			System.out.println("|Escolha uma opção: |");
+			System.out.println("|1 - Soma           |");
+			System.out.println("|2 - Subtração      |");
+			System.out.println("|3 - Divisão        |");
+			System.out.println("|4 - Multiplicação  |");
+			System.out.println("+-------------------+");
 			opcao = leitor.nextInt();
+			
+			while (opcao > 4 || opcao < 1) {
+				System.out.println("Você digitou um número de opção inválido");
+				System.out.print("Escolha uma opção: 1 - soma; 2 - subtração; 3 - divisão; 4- multiplicação");
+				opcao = leitor.nextInt();
+			}
+			
+			System.out.print("Digite o primeiro número: ");
+			num1 = leitor.nextDouble();
+			System.out.print("Digite o segundo número: ");
+			num2 = leitor.nextDouble();
+				
+			switch (opcao) {
+			case 1:
+				resultado = num1 + num2;
+				break;
+				
+			case 2:
+				resultado = num1 - num2;
+				break;
+				
+			case 3:
+				resultado = num1 / num2;
+				break;
+				
+			case 4:
+				resultado = num1 * num2;
+				break;
+			}
+			System.out.println("resultado =" + resultado );		
+			System.out.println("Deseja executar outra operação? yes = 1/no = 2" );
+			continuarC = leitor.nextInt();
+			
+			if(continuarC == 1) {
+				repeat = true;
+			} else {
+				System.out.println("obrigado pela preferencia");
+				repeat = false;
+			}
 		}
-		
-		System.out.print("Digite o primeiro número: ");
-		num1 = leitor.nextDouble();
-		System.out.print("Digite o segundo número: ");
-		num2 = leitor.nextDouble();
-		
-		leitor.close();
-		
-		switch (opcao) {
-		case 1:
-			resultado = num1 + num2;
-			break;
 			
-		case 2:
-			resultado = num1 - num2;
-			break;
-			
-		case 3:
-			resultado = num1 / num2;
-			break;
-			
-		case 4:
-			resultado = num1 * num2;
-			break;
-		}
-		
-		System.out.print("resultado = " + resultado);
+			leitor.close();
 	}
 }
